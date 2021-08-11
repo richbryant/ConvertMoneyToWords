6ed5328c9b2ee1fba622955c92f152523097fabe
Rich <richard.bryant2@conduent.com>
2021-08-11 22:28:55 +0100

    Far more functional :)

--- ConvertMoneyToWords.Tests/FunctionTests.cs
+++ ConvertMoneyToWords.Tests/FunctionTests.cs
@@ -84,202 +84,27 @@ namespace ConvertMoneyToWords.Tests
             result.ShouldBe(expectedResult);
         }
 
-        [TestCase(55, "Fifty")]
-        [TestCase(77, "Seventy")]
-        public void TestGetTens(int value, string expectedResult)
-        {
-            var result = value.GetTens();
-
-            result.ShouldBeOfType<string>();
-            result.ShouldBe(expectedResult);
-        }
-
-        [TestCase(55, "Five")]
-        [TestCase(77, "Seven")]
-        public void TestGetDigits(int value, string expectedResult)
-        {
-            var result = value.GetDigits();
-
-            result.ShouldBeOfType<string>();
-            result.ShouldBe(expectedResult);
-        }
-
-        [TestCase(55, 5)]
-        [TestCase(77, 7)]
-        [TestCase(1999412, 2)]
-        public void TestGetLastDigit(int value, int expectedResult)
-        {
-            var result = value.GetLastDigit();
-
-            result.ShouldBeOfType<int>();
-            result.ShouldBe(expectedResult);
-        }
-
-        [TestCase(535, 5)]
-        [TestCase(737, 7)]
-        [TestCase(1999412, 1)]
-        public void TestGetFirstDigit(int value, int expectedResult)
-        {
-            var result = value.GetFirstDigit();
-
-            result.ShouldBeOfType<int>();
-            result.ShouldBe(expectedResult);
-        }
-
-        [TestCase(30, "Thirty pence")]
-        [TestCase(19, "Nineteen pence")]
-        [TestCase(1, "One penny")]
-        [TestCase(0, "Zero pence")]
-        [TestCase(null, "Zero pence")]
-        [TestCase(74, "Seventy Four pence")]
-        public void TestPenceWords(int value, string expectedResult)
+        [TestCase(1, "one pound and zero pence")]
+        [TestCase(155, "one hundred and fifty-five pounds and zero pence")]
+        [TestCase(0, "zero pounds and zero pence")]
+        [TestCase(0.01, "zero pounds and one penny")]
+        [TestCase(5000, "five thousand pounds and zero pence")]
+        [TestCase(5555.06, "five thousand five hundred and fifty-five pounds and six pence")]
+        [TestCase(55555.55, "fifty-five thousand five hundred and fifty-five pounds and fifty-five pence")]
+        [TestCase(555555.80, "five hundred and fifty-five thousand five hundred and fifty-five pounds and eighty pence")]
+        [TestCase(5555555.99, "five million five hundred and fifty-five thousand five hundred and fifty-five pounds and ninety-nine pence")]
+        [TestCase(55555555, "fifty-five million five hundred and fifty-five thousand five hundred and fifty-five pounds and zero pence")]
+        [TestCase(555555555.55, "five hundred and fifty-five million five hundred and fifty-five thousand five hundred and fifty-five pounds and fifty-five pence")]
+        [TestCase(5000000.01, "five million pounds and one penny")]
+        [TestCase(5005005, "five million five thousand and five pounds and zero pence")]
+        [TestCase(505000505, "five hundred and five million five hundred and five pounds and zero pence")]
+        public void TestPoundWords(decimal value, string expectedResult)
         {
             var example = Some(value);
-            var result = example.PenceWords();
+            var result = example.Map(x => Functions.GetWords(x.ToString()));
 
             result.ShouldBeOfType<Option<string>>();
             result.ShouldBe(expectedResult);
         }
-
-        [TestCase(1, 1)]
-        [TestCase(11, 11)]
-        [TestCase(123, 23)]
-        [TestCase(12345, 45)]
-        public void TestGetTensOption(int value, int expectedResult)
-        {
-            var example = Some(value);
-
-            var result = example.GetTens();
-
-            result.ShouldBeOfType<Option<int>>();
-            result.ShouldBe(expectedResult);
-        }
-
-        [TestCase(123, 1)]
-        [TestCase(12345, 3)]
-        [TestCase(123456, 4)]
-        public void TestGetHundredsOption(int value, int expectedResult)
-        {
-            var example = Some(value);
-
-            var result = example.GetHundreds();
-
-            result.ShouldBeOfType<Option<int>>();
-            result.ShouldBe(expectedResult);
-        }
-
-        [TestCase(1234, 1)]
-        [TestCase(12345, 12)]
-        [TestCase(123456, 123)]
-        [TestCase(1234567, 234)]
-        public void TestGetThousandsOption(int value, int expectedResult)
-        {
-            var example = Some(value);
-
-            var result = example.GetThousands();
-
-            result.ShouldBeOfType<Option<int>>();
-            result.ShouldBe(expectedResult);
-        }
-
-        [TestCase(1234123, 1)]
-        [TestCase(12345123, 12)]
-        [TestCase(123123456, 123)]
-        public void TestGetMillionsOption(int value, int expectedResult)
-        {
-            var example = Some(value);
-
-            var result = example.GetMillions();
-
-            result.ShouldBeOfType<Option<int>>();
-            result.ShouldBe(expectedResult);
-        }
-
-        [TestCase(55, "Fifty Five pounds")]
-        [TestCase(19, "Nineteen pounds")]
-        [TestCase(30, "Thirty pounds")]
-        [TestCase(0, "Zero pounds")]
-        [TestCase(1, "One pound")]
-        public void TestGetTensText(int value, string expectedResult)
-        {
-            var result = value.TensText(value);
-
-            result.ShouldBeOfType<string>();
-            result.ShouldBe(expectedResult);
-        }
-
-        [TestCase(1, 100, "One hundred")]
-        [TestCase(5, 599, "Five hundred")]
-        [TestCase(8, 12806, "Eight hundred")]
-        [TestCase(0, 0,"")]
-        [TestCase(11, 11, "")]
-        public void TestHundredsText(int value, int total, string expectedResult)
-        {
-            var result = value.HundredsText(total);
-
-            result.ShouldBeOfType<string>();
-            result.ShouldBe(expectedResult);
-        }
-
-        [TestCase(1, "One million")]
-        [TestCase(19, "Nineteen million")]
-        [TestCase(134, "One hundred and Thirty Four million")]
-        [TestCase(0, "")]
-        [TestCase(1000, "")]
-        public void TestMillionsText(int value, string expectedResult)
-        {
-            var result = value.MillionsText(Some(value * 1000000));
-
-            result.ShouldBeOfType<string>();
-            result.ShouldBe(expectedResult);
-        }
-
-
-        
-        [TestCase(55, 155, " and Fifty Five pounds")]
-        [TestCase(19, 719, " and Nineteen pounds")]
-        [TestCase(30, 1998430, " and Thirty pounds")]
-        [TestCase(55, 55, "Fifty Five pounds")]
-        [TestCase(19, 19, "Nineteen pounds")]
-        [TestCase(30, 30, "Thirty pounds")]
-        [TestCase(05, 105, " and Five pounds")]
-        [TestCase(5, 5, "Five pounds")]
-        [TestCase(0, 0, "Zero pounds")]
-        [TestCase(1, 1, "One pound")]
-        public void TestGetTensWithAnd(int value, int total, string expectedResult)
-        {
-            var result = value.TensText(total);
-            
-            result.ShouldBeOfType<string>();
-            result.ShouldBe(expectedResult);
-        }
-
-        [TestCase(1, "One pound")]
-        [TestCase(155, "One hundred and Fifty Five pounds")]
-        [TestCase(0, "Zero pounds")]
-        [TestCase(5000, "Five thousand pounds")]
-        [TestCase(5555, "Five thousand, Five hundred and Fifty Five pounds")]
-        [TestCase(55555, "Fifty Five thousand, Five hundred and Fifty Five pounds")]
-        [TestCase(555555, "Five hundred and Fifty Five thousand, Five hundred and Fifty Five pounds")]
-        [TestCase(5555555, "Five million, Five hundred and Fifty Five thousand, Five hundred and Fifty Five pounds")]
-        [TestCase(55555555, "Fifty Five million, Five hundred and Fifty Five thousand, Five hundred and Fifty Five pounds")]
-        [TestCase(555555555, "Five hundred and Fifty Five million, Five hundred and Fifty Five thousand, Five hundred and Fifty Five pounds")]
-        [TestCase(5000000, "Five million pounds")]
-        [TestCase(5005005, "Five million, Five thousand and Five pounds")]
-        [TestCase(505000505, "Five hundred and Five million, Five hundred and Five pounds")]
-        public void TestPoundWords(int value, string expectedResult)
-        {
-            var example = Some(value);
-            var result = example.PoundWords();
-
-            result.ShouldBeOfType<Option<string>>();
-            result.ShouldBe(expectedResult);
-        }
-
-
-
-        //
-        
-        //
     }
 }
--- ConvertMoneyToWords/Functions.cs
+++ ConvertMoneyToWords/Functions.cs
@@ -1,38 +1,46 @@
-﻿using ConvertMoneyToWords.Enums;
-using System;
+﻿using System;
 using System.Globalization;
+using Humanizer;
 using LanguageExt;
 using static LanguageExt.Prelude;
-// ReSharper disable AccessToModifiedClosure
 
 
 namespace ConvertMoneyToWords
 {
     public static class Functions
     {
-        public static string GetWords(string value)
+        public static string GetWords(string number)
         {
-            var response = "";
-            var workingValue = SafeNumber(value);
-            var workingPounds = SafePounds(workingValue);
-            var workingPence = SafePence(workingValue);
+            var value = SafeNumber(number);
+            var workingPounds = SafePounds(value);
+            var workingPence = SafePence(value);
 
-            var poundWords = "";
-            var penceWords = "";
+            var pounds = workingPounds.Bind(x => FormatPluralPounds(x));
+            var pence = workingPence.Bind(x => FormatPluralPence(x));
 
-            workingPounds.PoundWords().Match(
-                Some: x => poundWords += x,
-                None: () => { });
+            if (workingPounds < 0)
+            {
+                return pence.Match(Some: x => x, None: () => "Invalid entry");
+            }
 
-            workingPence.PenceWords().Match(
-                Some: x => penceWords += x,
-                None: () => { });
+            if (workingPence < 0)
+            {
+                return pounds.Match(x => x, () => "Invalid entry");
+            }
 
-            response += poundWords;
-            if (!string.IsNullOrEmpty(poundWords) && !string.IsNullOrEmpty(penceWords)) response += " and " + penceWords;
-
-            return response;
+            return pounds.Match(x => x, () => "") + " and " + pence.Match(x => x, () => "");
         }
+
+        private static Option<string> FormatPluralPounds(Option<int> number)
+            => number.IsSome && number == 1
+                    ? number.AsWords().Map(x => x + " pound")
+                    : number.AsWords().Map(x => x + " pounds");
+
+        private static Option<string> FormatPluralPence(Option<int> number)
+            => number.IsSome && number == 1
+                ? number.AsWords().Map(x => x + " penny")
+                : number.AsWords().Map(x => x + " pence");
+        
 
         public static Option<decimal> SafeNumber(string value)
             => parseDecimal(value).IfNone(0);
@@ -51,240 +59,11 @@ namespace ConvertMoneyToWords
             => parseDecimal(value).IfNone(0);
 
         public static int GetPence(this Option<decimal> value)
-        {
-            var result = 0;
-            value.Match(
-                x=> result = Convert.ToInt32(x % 1 * 100),
-                () => result = 0);
-            return result;
-        }
-
-        public static decimal GetDecimals(this Option<decimal> value)
-        {
-            var result = 0M;
-            value.Match(
-                x=> result = x - Math.Floor(x),
-                () => result = 0M);
-                
-            return result;
-        }
-
-        public static string GetTens(this int value)
-        { 
-            var number = value.GetFirstDigit() * 10;
-            return number.ToEnum<BaseNumbers>().ToString();
-        }
-
-        public static string GetDigits(this int value)
-            => value.GetLastDigit()
-                .ToEnum<BaseNumbers>()
-                .ToString();
-
-        public static int GetLastDigit(this int value)
-            => value % 10;
+            => value.Match(x=> Convert.ToInt32(x % 1 * 100),
+                () => 0);
         
-        public static int GetFirstDigit(this int value)
-        {
-            var result = value;
-            while (result >= 10)
-            {
-                result /= 10;
-            }
-
-            return result;
-        }
-
-        public static Option<string> PenceWords(this Option<int> value)
-        {
-            var response = "";
-            value.Filter(x => x >= 20 && x % 10 == 0)
-                .Map(x => response += x.GetTens());
-            value.Filter(x => x >= 20 && x % 10 != 0)
-                .Map(x => response += x.GetTens() + " " + x.GetDigits());
-            value.Filter(x => x < 20)
-                .Map(x => response += x.ToEnum<BaseNumbers>().ToString());
-            return response == "One" ? response += " penny" : response += " pence";
-        }
-
-
-        public static Option<int> GetTens(this Option<int> value)
-        {
-            var stringVal = "";
-            value.Match(x => stringVal = x.ToString(),
-                () => stringVal = "0");
-
-            var length = 2;
-            if (length > stringVal.Length) length = stringVal.Length;
-            if(stringVal.Length < 1) return Option<int>.None;
-
-            return parseInt(stringVal
-                .AsSpan()
-                .Slice(stringVal.Length - length, length)
-                .ToString()).IfNone(0);
-        }
-
+        public static Option<string> AsWords(this Option<int> value)
+            => value.Map(x => x.ToWords());
         
-        /// <summary> Gets the last 3 or fewer digits of the integer </summary>
-        public static Option<int> GetHundreds(this Option<int> value)
-        {
-            var stringVal = "";
-            value.Match(x => stringVal = x.ToString(),
-                () => stringVal = "0");
-
-            var length = 1;
-            if (length > stringVal.Length) length = stringVal.Length;
-            if(stringVal.Length < 3) return Option<int>.None;
-
-            return parseInt(stringVal
-                .AsSpan()
-                .Slice(stringVal.Length - 3, length)
-                .ToString());
-        }
-
-        
-
-        /// <summary> Gets the count of thousands in the integer </summary>
-        public static Option<int> GetThousands(this Option<int> value)
-        {
-            var stringval = "";
-            value.Match(x => stringval = x.ToString(),
-                () => stringval = "0");
-
-            var length = 1;
-            if(stringval.Length > 4) length += stringval.Length - 4;
-            if(length > 3) length = 3;
-            if(stringval.Length < 4) return Option<int>.None;
-
-            return parseInt(stringval
-                .AsSpan()
-                .Slice(stringval.Length - (length + 3), length)
-                .ToString());
-        }
-
-        public static Option<int> GetMillions(this Option<int> value)
-        {
-            var stringval = "";
-            value.Match(x => stringval = x.ToString(),
-                () => stringval = "0");
-            var length = 1;
-            if(stringval.Length > 7) length += stringval.Length - 7;
-            if (length > stringval.Length) length = stringval.Length;
-            if(stringval.Length < 7) return Option<int>.None;
-            if(length > 3) length = 3;
-
-            return parseInt(stringval
-                .AsSpan()
-                .Slice(stringval.Length - (length + 6), length)
-                .ToString());
-        }
-
-        public static string HundredsText(this int value, Option<int> total)
-        {
-            if(value < 1 || value > 9) return string.Empty;
-            var response = $"{(BaseNumbers) value.GetFirstDigit()}";
-            return response + " hundred";
-        }
-
-        public static string ThousandsText(this int value, Option<int> total)
-        {
-            if(value > 999 || value < 1) return string.Empty;
-            var value1 = value * 1000;
-            
-            var xValue = total.Map(x => (x - value1) / 1000M);
-            var hasHundreds = false;
-            total.Match(
-                Some: x => hasHundreds = Some(x/100M).GetDecimals() > 0.1M,
-                None: () => hasHundreds = false);
-
-            var response = "";
-            var noAnd = true;
-            if (value > 99)
-            {
-                noAnd = false;
-                response += value.GetFirstDigit().HundredsText(value);
-
-                value = Convert.ToInt32(value.ToString().AsSpan().Slice(1, 2).ToString());
-                if(value == 0) noAnd = true;
-            }
-
-            if(value > 0)
-            {
-                if(!noAnd) response += " and ";
-                var rText = $"{value.TensText(value).Replace(" pounds", "").Replace(" pound", "")}";
-                if (!rText.Contains("Zero"))
-                {
-                    response += rText;
-                }
-            }
-            response += " thousand";
-            
-            if (xValue.GetDecimals() > 0 && hasHundreds) response += ", ";
-
-            return response;
-        }
-
-        public static string MillionsText(this int value, Option<int> total)
-        {
-            if(value > 999 || value < 1) return string.Empty;
-            var value1 = Convert.ToDecimal(value * 1000000M);
-
-            var xValue = total.Map(x => (x - value1) / 1000000M);
-
-            var response = "";
-            var noAnd = true;
-            if (value > 99)
-            {
-                noAnd = false;
-                response += value.GetFirstDigit().HundredsText(value);
-
-                value = Convert.ToInt32(value.ToString().AsSpan().Slice(1, 2).ToString());
-                if(value == 0) noAnd = true;
-            }
-
-            if(value > 0)
-            {
-                if(!noAnd) response += " and ";
-                response += $"{value.TensText(value).Replace(" pounds", "").Replace(" pound", "")}";
-            }
-            response += " million";
-
-            if (xValue.GetDecimals() > 0) response += ", ";
-
-            return response;
-        }
-
-
-        public static string TensText(this int value, Option<int> total)
-        {                  
-            var resultString = "";
-            if(value <= 20 || value.GetLastDigit() == 0) resultString = $"{value.ToEnum<BaseNumbers>().ToString()}";
-
-            if(value.GetDigits() == "Zero") resultString = $"{value.ToEnum<BaseNumbers>().ToString()}";
-            
-            if(value > 20 && value.GetLastDigit() != 0) resultString = $"{value.GetTens()} {value.GetDigits()}";
-
-            if (value == 0 && total > 99) resultString = "";
-
-            if(total > 99 && value != 0)
-            {
-                resultString = " and " + resultString;
-            }
-
-            resultString += total == 1 ? " pound" : " pounds";
-
-            return resultString;
-        }
-
-
-        public static Option<string> PoundWords(this Option<int> value)
-        {
-            var response = "";
-            value.GetMillions().IfSome(x => response += $"{x.MillionsText(value)}");
-            value.GetThousands().IfSome(x => response += $"{x.ThousandsText(value)}");
-            value.GetHundreds().IfSome(x => response += $"{x.HundredsText(value)}");
-            value.GetTens().IfSome(x => response += $"{x.TensText(value)}");
-
-            return response;
-        }
     }
 }
